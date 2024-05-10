ea8a68b8ea096ca3aa72df7309b790123337a9a0
Babak K. Shandiz <babak.k.shandiz@canonical.com>
2024-05-10 12:57:22 +0100

    Add `oci_image` function
    Signed-off-by: Babak K. Shandiz <babak.k.shandiz@canonical.com>

--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -37,20 +37,34 @@ async def get_unit_url(ops_test: OpsTest, application, unit, port, protocol="htt
     return f"{protocol}://{url}:{port}"
 
 
-async def perform_livepatch_integrations(ops_test: OpsTest):
-    """Add relations between Livepatch charm, postgresql-k8s, and nginx-ingress.
+def oci_image(metadata_file: str, image_name: str) -> str:
+    """Find upstream source for a container image.
 
     Args:
-        ops_test: PyTest object.
+        metadata_file: string path of metadata YAML file relative
+            to top level charm directory
+        image_name: OCI container image string name as defined in
+            metadata.yaml file
+
+    Returns:
+        upstream image source
+
+    Raises:
+        FileNotFoundError: if metadata_file path is invalid
+        ValueError: if upstream source for image name can not be found
     """
-    logger.info("Integrating Livepatch and Postgresql")
-    await ops_test.model.integrate(f"{APP_NAME}:database", f"{POSTGRESQL_NAME}:database")
-    await ops_test.model.integrate(f"{APP_NAME}:ingress", f"{NGINX_INGRESS_CHARM_NAME}:ingress")
+    metadata = yaml.safe_load(Path(metadata_file).read_text())
 
-    def checker():
-        return (
-            "Waiting for postgres relation"
-            not in ops_test.model.applications[APP_NAME].units[0].workload_status_message
-        )
+    resources = metadata.get("resources", {})
+    if not resources:
+        raise ValueError("No resources found")
 
-    await ops_test.model.block_until(checker)
+    image = resources.get(image_name, {})
+    if not image:
+        raise ValueError(f"{image_name} image not found")
+
+    upstream_source = image.get("upstream-source", "")
+    if not upstream_source:
+        raise ValueError("Upstream source not found")
+
+    return upstream_source

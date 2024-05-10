bde10bb514adb0bb565887ad41b487fbbebbeb7e
Babak K. Shandiz <babak.k.shandiz@canonical.com>
2024-05-10 12:59:10 +0100

    Remove test skip decorator
    Signed-off-by: Babak K. Shandiz <babak.k.shandiz@canonical.com>

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -6,9 +6,9 @@ import logging
 
 import pytest
 import requests
-from conftest import APP_NAME
-from ops.model import ActiveStatus
 from pytest_operator.plugin import OpsTest
+
+from helpers import ACTIVE_STATUS, APP_NAME
 
 logger = logging.getLogger(__name__)
 
@@ -18,18 +18,16 @@ logger = logging.getLogger(__name__)
 class TestDeployment:
     """A wrapper class for deployment tests"""
 
-    @pytest.mark.skip("Pass until the action that uploads the oci images as artifacts in the server is merged.")
     async def test_application_is_up(self, ops_test: OpsTest):
-        """The app is up and running."""
+        """Test the app is up and running."""
 
         logger.info("Getting model status")
         status = await ops_test.model.get_status()  # noqa: F821
         logger.info(f"Status: {status}")
-        assert ops_test.model.applications[APP_NAME].status == ActiveStatus.name
+        assert ops_test.model.applications[APP_NAME].status == ACTIVE_STATUS
 
-        address = status["applications"][APP_NAME]["units"][f"{APP_NAME}/0"]["address"]
-
-        url = f"http://{address}:8080/debug/status"
+        app_address = status["applications"][APP_NAME]["units"][f"{APP_NAME}/0"]["address"]
+        url = f"http://{app_address}:8080/debug/status"
         logger.info("Querying app address: %s", url)
         r = requests.get(url, timeout=2.0)
         assert r.status_code == 200

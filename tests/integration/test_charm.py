73c080101920d3e52191b762788a9206dcedf8f0
Babak K. Shandiz <babak.k.shandiz@canonical.com>
2024-05-10 14:21:00 +0100

    Remove test classes
    Signed-off-by: Babak K. Shandiz <babak.k.shandiz@canonical.com>

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -13,22 +13,18 @@ from helpers import ACTIVE_STATUS, APP_NAME
 logger = logging.getLogger(__name__)
 
 
-@pytest.mark.abort_on_fail
 @pytest.mark.usefixtures("deploy")
-class TestDeployment:
-    """A wrapper class for deployment tests"""
+async def test_application_is_up(self, ops_test: OpsTest):
+    """Test the app is up and running."""
 
-    async def test_application_is_up(self, ops_test: OpsTest):
-        """Test the app is up and running."""
+    logger.info("Getting model status")
+    status = await ops_test.model.get_status()  # noqa: F821
+    logger.info(f"Status: {status}")
+    assert ops_test.model.applications[APP_NAME].status == ACTIVE_STATUS
 
-        logger.info("Getting model status")
-        status = await ops_test.model.get_status()  # noqa: F821
-        logger.info(f"Status: {status}")
-        assert ops_test.model.applications[APP_NAME].status == ACTIVE_STATUS
-
-        app_address = status["applications"][APP_NAME]["units"][f"{APP_NAME}/0"]["address"]
-        url = f"http://{app_address}:8080/debug/status"
-        logger.info("Querying app address: %s", url)
-        r = requests.get(url, timeout=2.0)
-        assert r.status_code == 200
-        logger.info(f"Output = {r.json()}")
+    app_address = status["applications"][APP_NAME]["units"][f"{APP_NAME}/0"]["address"]
+    url = f"http://{app_address}:8080/debug/status"
+    logger.info("Querying app address: %s", url)
+    r = requests.get(url, timeout=2.0)
+    assert r.status_code == 200
+    logger.info(f"Output = {r.json()}")
--- tests/integration/test_upgrade.py
+++ tests/integration/test_upgrade.py
@@ -14,51 +14,47 @@ from helpers import ACTIVE_STATUS, APP_NAME
 logger = logging.getLogger(__name__)
 
 
-@pytest.mark.abort_on_fail
 @pytest.mark.usefixtures("deploy_current_stable")
-class TestUpgrade:
-    """A wrapper class for upgrade tests"""
+async def test_upgrade(self, ops_test: OpsTest):
+    """Test upgrading from the current stable release works as expected."""
 
-    async def test_upgrade(self, ops_test: OpsTest):
-        """Test upgrading from the current stable release works as expected."""
+    logger.info("Getting model status")
+    status = await ops_test.model.get_status()  # noqa: F821
+    logger.info(f"Status: {status}")
+    assert ops_test.model.applications[APP_NAME].status == ACTIVE_STATUS
 
-        logger.info("Getting model status")
-        status = await ops_test.model.get_status()  # noqa: F821
-        logger.info(f"Status: {status}")
-        assert ops_test.model.applications[APP_NAME].status == ACTIVE_STATUS
+    address = status["applications"][APP_NAME]["units"][f"{APP_NAME}/0"]["address"]
+    url = f"http://{address}:8080/debug/status"
+    logger.info("Querying app address: %s", url)
+    r = requests.get(url, timeout=2.0)
+    assert r.status_code == 200
+    logger.info(f"Output = {r.json()}")
 
-        address = status["applications"][APP_NAME]["units"][f"{APP_NAME}/0"]["address"]
-        url = f"http://{address}:8080/debug/status"
-        logger.info("Querying app address: %s", url)
-        r = requests.get(url, timeout=2.0)
-        assert r.status_code == 200
-        logger.info(f"Output = {r.json()}")
+    # Deploy the locally built charm and wait for active/idle status
+    logger.info("refreshing running application with the new local charm")
 
-        # Deploy the locally built charm and wait for active/idle status
-        logger.info("refreshing running application with the new local charm")
+    charm = await fetch_charm(ops_test)
+    await ops_test.model.applications[APP_NAME].refresh(
+        path=charm,
+        resources=get_charm_resources(),
+    )
 
-        charm = await fetch_charm(ops_test)
-        await ops_test.model.applications[APP_NAME].refresh(
-            path=charm,
-            resources=get_charm_resources(),
+    logger.info("waiting for the upgraded unit to be ready")
+    async with ops_test.fast_forward():
+        await ops_test.model.wait_for_idle(
+            apps=[APP_NAME],
+            status=ACTIVE_STATUS,
+            timeout=600,
         )
 
-        logger.info("waiting for the upgraded unit to be ready")
-        async with ops_test.fast_forward():
-            await ops_test.model.wait_for_idle(
-                apps=[APP_NAME],
-                status=ACTIVE_STATUS,
-                timeout=600,
-            )
+    logger.info("Getting model status after upgrade")
+    status = await ops_test.model.get_status()  # noqa: F821
+    logger.info(f"Status: {status}")
+    assert ops_test.model.applications[APP_NAME].status == ACTIVE_STATUS
 
-        logger.info("Getting model status after upgrade")
-        status = await ops_test.model.get_status()  # noqa: F821
-        logger.info(f"Status: {status}")
-        assert ops_test.model.applications[APP_NAME].status == ACTIVE_STATUS
-
-        address = status["applications"][APP_NAME]["units"][f"{APP_NAME}/0"]["address"]
-        url = f"http://{address}:8080/debug/status"
-        logger.info("Querying app address: %s", url)
-        r = requests.get(url, timeout=2.0)
-        assert r.status_code == 200
-        logger.info(f"Output = {r.json()}")
+    address = status["applications"][APP_NAME]["units"][f"{APP_NAME}/0"]["address"]
+    url = f"http://{address}:8080/debug/status"
+    logger.info("Querying app address: %s", url)
+    r = requests.get(url, timeout=2.0)
+    assert r.status_code == 200
+    logger.info(f"Output = {r.json()}")

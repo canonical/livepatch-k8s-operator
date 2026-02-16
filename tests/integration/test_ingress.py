fb07e2d6a48ee0ea8125fc80f4b67f86ceb7ec74
Haydn Tamura <haydn.tamura@canonical.com>
2026-02-16 16:41:32 +0000

    Full test workflow for ingress related tests

--- tests/integration/test_ingress.py
+++ tests/integration/test_ingress.py
@@ -27,3 +27,18 @@ async def test_traefik_ingress_relation(ops_test: OpsTest):
     logger.info("Querying app address: %s", url)
     response = requests.get(url, timeout=2.0)
     assert response.status_code == 200
+
+@pytest.mark.asyncio
+async def test_nginx_ingress_relation(ops_test: OpsTest):
+    """Test the charm integrates with nginx-route."""
+    await deploy_package(ops_test, ingress_method="nginx-route")
+
+    status = await ops_test.model.get_status()  # noqa: F821
+    assert TRAEFIK_K8S_NAME not in status["applications"]
+    assert ops_test.model.applications[APP_NAME].status == ACTIVE_STATUS
+
+    app_address = status["applications"][APP_NAME]["units"][f"{APP_NAME}/0"]["address"]
+    url = f"http://{app_address}:8080/debug/status"
+    logger.info("Querying app address: %s", url)
+    response = requests.get(url, timeout=2.0)
+    assert response.status_code == 200
